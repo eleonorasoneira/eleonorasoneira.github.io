b49f1a156c4a9ad8009d4a63dd97e9ea066fe619
María Eleonora Soneira <75816494+eleonorasoneira@users.noreply.github.com>
2026-09-16 08:59:45 -0300

    home

--- src/components/commons/socialIcons/socialIcons.jsx
+++ src/components/commons/socialIcons/socialIcons.jsx
@@ -1,52 +1,31 @@
 import {
-  FaXTwitter,
   FaLinkedinIn,
   FaGithub,
-  FaInstagram,
 } from "react-icons/fa6";
 
 const SocialIcons = () => {
   return (
     <div className="my-5 flex items-center justify-center">
       <a
-        href="https://www.linkedin.com/in/lautaro-rodr%C3%ADguez-collins-40505624a/"
+        href="https://www.linkedin.com/in/eleonorasoneira/"
         target="_blank"
         rel="noopener noreferrer"
-        className="mr-3 text-zinc-400 hover:text-[#69c7c7] drop-shadow-[1px_1px_0_#7836cf]"
+        className="mr-3 text-zinc-400 hover:text-[#69c7c7] drop-shadow-[1px_1px_0_#00B8D4]"
         aria-label="Linkedin"
       >
         <FaLinkedinIn className="h-7 w-7" />
       </a>
 
       <a
-        href="https://github.com/Lautaro-R-collins"
+        href="https://github.com/eleonorasoneira"
         target="_blank"
         rel="noopener noreferrer"
-        className="mx-3 text-zinc-400 hover:text-[#69c7c7] drop-shadow-[1px_1px_0_#7836cf]"
+        className="mx-3 text-zinc-400 hover:text-[#69c7c7] drop-shadow-[1px_1px_0_#00B8D4]"
         aria-label="Github"
       >
         <FaGithub className="h-7 w-7" />
       </a>
 
-      <a
-        href="https://x.com/lautarodevelops/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="mx-3 text-zinc-400 hover:text-[#69c7c7] drop-shadow-[1px_1px_0_#7836cf]"
-        aria-label="Twitter"
-      >
-        <FaXTwitter className="h-7 w-7" />
-      </a>
-
-      <a
-        href="https://www.instagram.com/lautarodevelops/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="mx-3 text-zinc-400 hover:text-[#69c7c7] drop-shadow-[1px_1px_0_#7836cf]"
-        aria-label="Instagram"
-      >
-        <FaInstagram className="h-7 w-7" />
-      </a>
     </div>
   );
 };
